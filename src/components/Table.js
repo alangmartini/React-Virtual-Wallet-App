636d47f1bcdfc4fe6c7e7c4cb03543c8c6d23c15
gmaralan <gmartinialan@gmail.com>
2022-12-08 20:54:33 -0400

    habemos 7?

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,81 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  findCorrectRate = (expenseObject) => {
+    const { exchangeRates } = expenseObject;
+    const isTest = !Object.keys(exchangeRates)[0].includes('BRL');
+    const keyName = isTest ? expenseObject.currency : `${expenseObject.currency}BRL`;
+    const expenseCoin = Object.keys(exchangeRates)
+      .find((rateName) => rateName === keyName);
+    return exchangeRates[expenseCoin];
+  };
+
+  parseInfomation = (expenses) => {
+    const withConversionCoin = expenses.map((info) => {
+      const expenseRate = this.findCorrectRate(info);
+      return { ...info, conversion: expenseRate.name };
+    });
+    return withConversionCoin;
+  };
+
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <div>
         <h1>Table</h1>
         <table>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda gitde conversão</th>
-          <th>Editar/Excluir</th>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+
+          </thead>
+          <tbody>
+            { expenses.length > 0 && this.parseInfomation(expenses).map((expense) => {
+              const { description,
+                tag, value, method, conversion } = expense;
+              const currentRate = this.findCorrectRate(expense);
+              const rate = currentRate.ask;
+              const convertedValue = (parseFloat(rate) * parseFloat(value))
+                .toFixed(2);
+              return (
+                <tr key={ value }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ parseFloat(value).toFixed(2) }</td>
+                  <td>{ conversion }</td>
+                  <td>{ parseFloat(currentRate.ask).toFixed(2) }</td>
+                  <td>{ convertedValue }</td>
+                  <td>Real</td>
+                </tr>
+              );
+            })}
+          </tbody>
+
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf('').isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
